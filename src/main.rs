880dab26f65d203614692394dedab39452b025f7
YiChun Sung <ycsung.r@gmail.com>
2022-03-03 02:01:04 +0800

    Added loop

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,103 @@ fn main() {
     println!("{}", hello_world);
     let data = hello(x);
     println!("{}", data);
+    data_type();
+    tuples_and_array();
+    hello_shadowing();
+    if_else_function();
+    loop_function();
+}
+
+fn loop_function() {
+    let mut number_data: u32 = 0;
+     loop {
+        if number_data >= 10 {
+            break;
+        }
+
+        println!("Hello: {}", number_data);
+        number_data += 1;
+
+    }
+}
+
+fn if_else_function() {
+    let number: u32 = 2;
+    if number > 30 {
+        println!("{}", "hello");
+    } else {
+        println!("{:?}", "nonononono");
+    }
+
+    if number == 10 {
+        println!("{}", "toto");
+    } else if number == 2 {
+        println!("{}", number == 2);
+    }
+
+    let data_menu = if number == 2 { 5 } else { 10 };
+
+    println!("{}", data_menu);
+}
+
+// 遮蔽
+fn hello_shadowing() {
+    let x: i32 = 33;
+    let y = {
+        let x = x + 2;
+        println!("{:?}", x);
+        x + 3
+    };
+    println!("{:?}", y);
 }
 
 fn hello(arg: u64) -> u64 {
-  return arg + 2;
+    return arg + 2;
 }
+
+fn data_type() {
+    let mut integer: u64 = 30;
+    integer = integer + 3;
+    println!("{:?}", integer);
+    const SIGN_DATA: i64 = -22;
+    println!("{:?}", SIGN_DATA);
+
+    let hello_float: f32 = 20.22;
+    println!("{:?}", hello_float);
+
+    let hello_boolean: bool = true;
+    println!("{:?}", hello_boolean);
+
+    // 字元
+    let char_data: char = 'y';
+    println!("{:?}", char_data);
+}
+
+fn tuples_and_array() {
+
+    // tuples
+    let mut tup: (i32, i32, bool) = (500, 500, false);
+    println!("{:?}", tup);
+
+    tup.1 = 5566;
+
+    let (a, b, c) = tup;
+    println!("{:?} {:?} {:?}", a, b, c);
+
+    let first_data = tup.2;
+    println!("{:?}", first_data);
+
+    // Array
+    let new_array = [true, false, true];
+    println!("{:?}", new_array);
+
+    let mut hello_array: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("{:?}", hello_array);
+    hello_array = [100, 200, 330, 444, 5566];
+    println!("{:?}", hello_array);
+
+    let data = [true; 5];
+    println!("{:?}", data);
+    println!("洗洗睡：{}", hello_array[4]);
+}
+
